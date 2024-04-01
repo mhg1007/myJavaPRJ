491b6233f0913ebc5c1076715b906aff26d736d7
mhg1007 <bout77@naver.com>
2024-04-01 14:00:40 +0900

    Code05_02_p

--- src/main/java/Code05_02.java
+++ src/main/java/Code05_02.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class Code05_02 {
     public static void main(String[] args) {
-        int num=99;
+        Scanner s=new Scanner(System.in);
+        System.out.print("숫자입력:");
+        int num=s.nextInt();
 
         if (num<100){
             System.out.print("100보다 ");
