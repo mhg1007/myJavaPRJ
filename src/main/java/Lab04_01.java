2599c8d813a0da7c20ec9a07a8ae52750385761c
mhg1007 <bout77@naver.com>
2024-04-01 09:26:51 +0900

    Lab04_01(문자열 거꾸로 출력하기)

---
+++ src/main/java/Lab04_01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Lab04_01 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+
+        System.out.println("문자를 입력하세요: ");
+        String ss=s.next();
+        int len=ss.length();
+
+        System.out.println("반대 문자열: ");
+        for (int i=len-1; i>=0; i--){
+            System.out.print(ss.charAt(i));
+        }
+    }
+}
