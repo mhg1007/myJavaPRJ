7d64efb3ede049a3efbc9162bb56827a10d70c7c
mhg1007 <bout77@naver.com>
2024-04-05 08:47:47 +0900

    programmers_P2

---
+++ src/main/java/programmers/P2.java
@@ -0,0 +1,21 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class P2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        char[] ch = a.toCharArray();
+        for (char c : ch) {
+
+            if (Character.isUpperCase(c)) {
+                c = Character.toLowerCase(c);
+            } else {
+                c = Character.toUpperCase(c);
+            }
+            System.out.print(c);
+        }
+    }
+}
