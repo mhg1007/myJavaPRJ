575674c8049d02b8c0b312972159237178dd2446
mhg1007 <bout77@naver.com>
2024-04-04 09:00:32 +0900

    P1_programmers연습문제

---
+++ src/main/java/programmers/P1.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class P1 {
+
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.println("홀수면 n 이하의 홀수인 모든 양수의 합");
+        System.out.println("짝수면 n 이하의 짝수인 모든 양수의 제곱의 합");
+        System.out.print("양의 정수 n을 입력하세요: ");
+        int n=s.nextInt();
+        System.out.println("결과: "+solution(n));
+
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        if(n%2!=0){
+            for(int i=n; i>0; i=i-2){
+                answer+=i;
+            }
+        }
+        else{
+            for(int i=n; i>0; i=i-2){
+                answer+=i*i;
+            }
+        }
+        return answer;
+    }
+}
