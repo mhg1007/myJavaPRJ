bf5a97ab02c02a695dbc4cbafb81354a0d42e882
mhg1007 <bout77@naver.com>
2024-04-05 17:13:29 +0900

    baekjoon1001

---
+++ src/main/java/baekjoon/B1001.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B1001 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int A=s.nextInt();
+        int B=s.nextInt();
+        System.out.print(A-B);
+    }
+}
