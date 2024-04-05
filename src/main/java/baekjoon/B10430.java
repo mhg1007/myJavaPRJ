39df0f051a706b98502ef87ba28e4f69687d5205
mhg1007 <bout77@naver.com>
2024-04-05 17:19:24 +0900

    baekjoon10430

---
+++ src/main/java/baekjoon/B10430.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B10430 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int A=s.nextInt();
+        int B=s.nextInt();
+        int C=s.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
