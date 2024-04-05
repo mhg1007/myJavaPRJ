81b7ec09d79fc73fa7b309748af056196b9f3dad
mhg1007 <bout77@naver.com>
2024-04-05 17:18:23 +0900

    baekjoon18108

---
+++ src/main/java/baekjoon/B18108.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B18108 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int y=s.nextInt();
+        System.out.print(y-543);
+    }
+}
