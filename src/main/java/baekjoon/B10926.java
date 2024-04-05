9d9dce0b894cec98d1cfdb0dbfa8ff1ea01b058c
mhg1007 <bout77@naver.com>
2024-04-05 17:17:26 +0900

    baekjoon10926

---
+++ src/main/java/baekjoon/B10926.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B10926 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String a=s.next();
+        System.out.print(a+"??!");
+    }
+}
