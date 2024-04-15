6be093ed34dd6ac17eaa728b64f096624b92e068
mhg1007 <bout77@naver.com>
2024-04-15 14:26:58 +0900

    Code06_09

---
+++ src/main/java/Code06_09.java
@@ -0,0 +1,14 @@
+public class Code06_09 {
+    public static void main(String[] args) {
+        for (int i=0; i<3; i++){
+            for(int k=0; k<2; k++){
+                System.out.println("난생처음은 쉽습니다.^^(i:"+i+",k:"+k+")");
+            }
+        }
+        for (int i = 2; i <=9 ; i++) {
+            for (int j = 1; j <=9 ; j++) {
+                System.out.println(i+" X "+j+" = "+i*j);
+            }
+        }
+    }
+}
