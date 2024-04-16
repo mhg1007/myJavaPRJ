2f29146b18f3bf2e89c70e52116b75374362daf2
mhg1007 <bout77@naver.com>
2024-04-17 08:43:16 +0900

    baekjoon11021

---
+++ src/main/java/baekjoon/B11021.java
@@ -0,0 +1,24 @@
+package baekjoon;
+import java.io.*;
+public class B11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        int T=Integer.parseInt(br.readLine());
+        String[] s=new String[T];
+
+        for (int i=0; i<T; i++){
+            String t=br.readLine();
+            String[] arr;
+            arr=t.split(" ");
+            int A=Integer.parseInt(arr[0]);
+            int B=Integer.parseInt(arr[1]);
+            s[i]= String.valueOf(A+B);
+            bw.write("Case #"+(i+1)+": ");
+            bw.write(s[i]);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
