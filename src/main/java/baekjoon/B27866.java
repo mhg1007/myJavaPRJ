47056c195716929fb6b2fba72bb4d9a3cc687723
mhg1007 <bout77@naver.com>
2024-05-06 13:52:56 +0900

    baekjoon27866

---
+++ src/main/java/baekjoon/B27866.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+public class B27866 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String S=s.next();
+        int i=s.nextInt();
+        System.out.print(S.charAt(i-1));
+    }
+}
