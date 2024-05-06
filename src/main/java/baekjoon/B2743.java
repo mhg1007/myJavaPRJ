b4465965cab23147cc6309775fd5540fb075a4d8
mhg1007 <bout77@naver.com>
2024-05-07 08:41:44 +0900

    baekjoon2743

---
+++ src/main/java/baekjoon/B2743.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+public class B2743 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String S=s.next();
+        int i=S.length();
+        System.out.print(i);
+    }
+}
