84d1b9d9e6a9ef6892b3b2ff1f7cec56ba74a771
mhg1007 <bout77@naver.com>
2024-05-07 15:10:14 +0900

    Code07_11

--- src/main/java/Code07_11.java
+++ src/main/java/Code07_11.java
@@ -3,9 +3,12 @@ public class Code07_11 {
         Rabbit rabbit1=new Rabbit("원");
         Rabbit rabbit2=new Rabbit("삼각형");
         Rabbit rabbit3=new Rabbit("토끼");
+        String rabbitshape="사각형";
+        Rabbit rabbit4=new Rabbit(rabbitshape);
 
         System.out.printf("토끼1 객체의 모양은 [%s]입니다.\n",rabbit1.shape);
         System.out.printf("토끼2 객체의 모양은 [%s]입니다.\n",rabbit2.shape);
         System.out.printf("토끼3 객체의 모양은 [%s]입니다.\n",rabbit3.shape);
+        System.out.printf("토끼4 객체의 모양은 [%s]입니다.\n",rabbit4.shape);
     }
 }
