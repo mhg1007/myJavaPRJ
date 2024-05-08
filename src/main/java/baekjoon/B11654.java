3fa3c5e4e3cd91ef643610d6dd48de540fac6931
mhg1007 <bout77@naver.com>
2024-05-09 08:55:20 +0900

    baekjoon11654

---
+++ src/main/java/baekjoon/B11654.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+public class B11654 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        char c=s.nextLine().charAt(0);
+        System.out.print((int)c);
+        s.close();
+    }
+}
