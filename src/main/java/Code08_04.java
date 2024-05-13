200d3763076f76db4d29960256d49fdfb7da459b
mhg1007 <bout77@naver.com>
2024-05-13 15:03:30 +0900

    Code08_04

---
+++ src/main/java/Code08_04.java
@@ -0,0 +1,5 @@
+public class Code08_04 {
+    public static void main(String[] args) {
+        HouseRabbit hRabbit=new HouseRabbit();
+    }
+}
--- src/main/java/Rabbit2.java
+++ src/main/java/Rabbit2.java
@@ -6,6 +6,7 @@ public class Rabbit2 {  //Code08
 
     Rabbit2(){
         count++;
+        System.out.println("1. 슈퍼 클래스(토끼)의 생성자가 호출됩니다.");
     }
     void move(int x,int y){
         this.xPos=x;
@@ -14,6 +15,9 @@ public class Rabbit2 {  //Code08
 }
 class HouseRabbit extends Rabbit2{
     String owner;
+    HouseRabbit(){
+        System.out.println("2. 서브 클래스(집토끼)의 생성자가 호출됩니다.");
+    }
     void eatFeed(){
         System.out.println("집토끼가 사료를 먹습니다.");
     }
