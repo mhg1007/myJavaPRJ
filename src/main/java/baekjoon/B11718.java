05fe9ba524183f185d8f43d3270e6aa81bb22b8e
mhg1007 <bout77@naver.com>
2024-05-20 09:00:26 +0900

    baekjoon11718

---
+++ src/main/java/baekjoon/B11718.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+public class B11718 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNext()) {
+            System.out.println(s.nextLine());
+        }
+    }
+}
