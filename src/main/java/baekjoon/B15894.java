34ff1e1893cdaae62607710460df05f185792ba5
mhg1007 <bout77@naver.com>
2024-06-28 07:12:15 +0900

    baekjoon15894

---
+++ src/main/java/baekjoon/B15894.java
@@ -0,0 +1,9 @@
+package baekjoon;
+import java.util.Scanner;
+public class B15894 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        long n=s.nextLong();
+        System.out.print(n*4);
+    }
+}
