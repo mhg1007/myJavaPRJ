ed8011884de34cd4288450757f70e3dde072663b
mhg1007 <bout77@naver.com>
2024-08-23 11:06:22 +0900

    baekjoon15439

---
+++ src/main/java/baekjoon/B15439.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+public class B15439 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        int p=N*N-N;
+        System.out.print(p);
+    }
+}
