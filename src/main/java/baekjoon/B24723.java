2feed32bce2c4bd8dcb6119c7d8b678002a5cd25
mhg1007 <bout77@naver.com>
2024-08-26 08:33:41 +0900

    baekjoon24723

---
+++ src/main/java/baekjoon/B24723.java
@@ -0,0 +1,9 @@
+package baekjoon;
+import java.util.Scanner;
+public class B24723 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        System.out.print((int)Math.pow(2,N));
+    }
+}
