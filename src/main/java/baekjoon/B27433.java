ab3538a702043d4c26a8eb14e9fc2aaf1a82483d
mhg1007 <bout77@naver.com>
2024-08-30 08:51:17 +0900

    baekjoon27433

---
+++ src/main/java/baekjoon/B27433.java
@@ -0,0 +1,17 @@
+package baekjoon;
+import java.util.Scanner;
+public class B27433 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        System.out.print(fact(N));
+    }
+    public static long fact(int n){
+        if(n<=1){
+            return 1;
+        }
+        else{
+            return n*fact(n-1);
+        }
+    }
+}
