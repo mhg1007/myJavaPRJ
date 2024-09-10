f069895c7586feea363493d86d3fc19cb400e44e
mhg1007 <bout77@naver.com>
2024-09-11 08:53:30 +0900

    baekjoon2446

---
+++ src/main/java/baekjoon/B2446.java
@@ -0,0 +1,30 @@
+package baekjoon;
+import java.util.Scanner;
+public class B2446 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N*2-i-1; j++) {
+                if(j>=i){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = N-2; i >=0; i--) {
+            for (int j = 0; j < N*2-i-1; j++) {
+                if(j>=i){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
