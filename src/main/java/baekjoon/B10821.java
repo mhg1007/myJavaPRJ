33b9760e19c631ea913873d3a0ca978ccde29e3a
mhg1007 <bout77@naver.com>
2024-11-13 08:50:54 +0900

    baekjoon10821

---
+++ src/main/java/baekjoon/B10821.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+public class B10821 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        String[] w=str.split(",");
+        System.out.print(w.length);
+    }
+}
