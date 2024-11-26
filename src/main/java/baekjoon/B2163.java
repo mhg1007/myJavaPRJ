ac2b6a54077802c63b6321efd9ef2561b275344f
mhg1007 <bout77@naver.com>
2024-11-27 08:52:49 +0900

    baekjoon2163

---
+++ src/main/java/baekjoon/B2163.java
@@ -0,0 +1,10 @@
+package baekjoon;
+import java.util.Scanner;
+public class B2163 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        int M=s.nextInt();
+        System.out.print((N*M)-1);
+    }
+}
