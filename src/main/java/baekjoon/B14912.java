749705434a91b9261501e9ac0078657248e6a28c
mhg1007 <bout77@naver.com>
2025-02-05 14:03:27 +0900

    baekjoon14912

---
+++ src/main/java/baekjoon/B14912.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B14912 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int d=s.nextInt();
+        int count=0;
+        for (int i = 1; i <=n ; i++) {
+            String str=String.valueOf(i);
+            for (int j = 0; j < str.length(); j++) {
+                if(Integer.parseInt(String.valueOf(str.charAt(j)))==d){
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
+    }
+}
