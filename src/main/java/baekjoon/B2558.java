eba0780bc9ee61d426e6384b841d4e37cf8ea352
mhg1007 <bout77@naver.com>
2025-06-26 07:57:31 +0900

    baekjoon2558

---
+++ src/main/java/baekjoon/B2558.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B2558 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int A=s.nextInt();
+        int B=s.nextInt();
+        System.out.print(A+B);
+    }
+}
