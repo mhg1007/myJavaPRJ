1eaa29584e6282267cc76ba09a3799a274b8f366
mhg1007 <bout77@naver.com>
2025-08-08 11:20:59 +0900

    baekjoon4101

---
+++ src/main/java/baekjoon/B4101.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B4101 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        while(s.hasNextInt()){
+            int a=s.nextInt();
+            int b=s.nextInt();
+            if(a==0&&b==0){
+                break;
+            }
+            System.out.println(a>b?"Yes":"No");
+        }
+    }
+}
