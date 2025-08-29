eb7fae16dddaa9da8849490e66cdfb875507fde8
mhg1007 <bout77@naver.com>
2025-08-29 10:00:47 +0900

    baekjoon5217

---
+++ src/main/java/baekjoon/B5217.java
@@ -0,0 +1,49 @@
+package baekjoon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class B5217 {
+    private static ArrayList<Integer> solveSumPairs(int target) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        /* ------------------- INSERT CODE HERE ---------------------*/
+
+        // answer.add(x);		// adds x to ArrayList<Integer> answer
+
+        for(int i=1; i<=target/2.0; i++){
+            if(i!=target-i){
+                answer.add(i);
+                answer.add(target-i);
+            }
+        }
+
+
+        /* -------------------- END OF INSERTION --------------------*/
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numCases = sc.nextInt();
+
+        for(int n = 0; n < numCases; n++)
+        {
+            int target = sc.nextInt();
+
+            ArrayList<Integer> answer = solveSumPairs(target);
+
+            System.out.print("Pairs for "+target+": ");
+
+            for (int i = 0; i<answer.size(); i+=2 ) {
+                if (i>0)
+                    System.out.print(", ");
+                System.out.print(answer.get(i) + " " + answer.get(i+1));
+            }
+            System.out.println();
+        }
+    }
+}
