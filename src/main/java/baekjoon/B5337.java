5dbdc1828744c515fe223dd4d79cd02daf8cf90c
mhg1007 <bout77@naver.com>
2025-09-02 08:39:08 +0900

    baekjoon5337

---
+++ src/main/java/baekjoon/B5337.java
@@ -0,0 +1,9 @@
+package baekjoon;
+
+public class B5337 {
+    public static void main(String[] args) {
+        System.out.print(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
