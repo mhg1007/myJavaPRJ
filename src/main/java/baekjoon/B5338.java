69c6a16d87ae299ebcf3d4c75d318e0b0ac71381
mhg1007 <bout77@naver.com>
2025-09-02 08:39:47 +0900

    baekjoon5338

---
+++ src/main/java/baekjoon/B5338.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class B5338 {
+    public static void main(String[] args) {
+        System.out.print("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
