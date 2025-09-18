45bfc31528b165f01d4730b9178287a54a9ff477
mhg1007 <bout77@naver.com>
2025-09-19 08:57:02 +0900

    baekjoon5717

---
+++ src/main/java/baekjoon/B5717.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B5717 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        while(true){
+            int a=s.nextInt();
+            int b=s.nextInt();
+            if(a==0&&b==0)
+                break;
+            System.out.println(a+b);
+        }
+    }
+}
