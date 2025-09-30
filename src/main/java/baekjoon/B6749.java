cfa46f526753fda81ed6d3240b22c76ad0b194c2
mhg1007 <bout77@naver.com>
2025-09-30 09:02:57 +0900

    baekjoon6749

---
+++ src/main/java/baekjoon/B6749.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B6749 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int Y=s.nextInt();
+        int M=s.nextInt();
+        System.out.print(M*2-Y);
+    }
+}
