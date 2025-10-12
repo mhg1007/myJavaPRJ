cb2fc8df359dd25fefb03b9fcd8561f7474fa98f
mhg1007 <bout77@naver.com>
2025-10-13 06:15:04 +0900

    baekjoon8370

---
+++ src/main/java/baekjoon/B8370.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B8370 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print((s.nextInt()*s.nextInt())+(s.nextInt()*s.nextInt()));
+    }
+}
