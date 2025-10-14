7dccd956d911bd36f4b4ea0bacbd12b6e244bcab
mhg1007 <bout77@naver.com>
2025-10-14 09:00:04 +0900

    baekjoon8545

---
+++ src/main/java/baekjoon/B8545.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B8545 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        StringBuilder sb=new StringBuilder();
+        sb.append(s.next());
+        sb.reverse();
+        System.out.print(sb);
+    }
+}
