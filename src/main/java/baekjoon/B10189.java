046f397c5e09862b20a323a805d3675724702b38
mhg1007 <bout77@naver.com>
2025-11-13 09:04:30 +0900

    baekjoon10189

---
+++ src/main/java/baekjoon/B10189.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+public class B10189 {
+    public static void main(String[] args) {
+        System.out.print("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
