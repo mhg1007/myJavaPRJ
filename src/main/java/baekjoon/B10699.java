24d5bd4e9663620ba3b7add172ad2688e4693d5c
mhg1007 <bout77@naver.com>
2025-11-17 08:40:29 +0900

    baekjoon10699

---
+++ src/main/java/baekjoon/B10699.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.time.LocalDate;
+
+public class B10699 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.print(now);
+    }
+}
