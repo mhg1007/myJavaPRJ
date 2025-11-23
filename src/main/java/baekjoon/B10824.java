748fca29e4db3efd97c6b10d67ef077272fff6ce
mhg1007 <bout77@naver.com>
2025-11-24 08:36:23 +0900

    baekjoon10824

---
+++ src/main/java/baekjoon/B10824.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B10824 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print(Long.parseLong(s.next()+s.next())+Long.parseLong(s.next()+s.next()));
+    }
+}
