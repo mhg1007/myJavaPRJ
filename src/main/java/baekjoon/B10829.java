4e831d176031b61e813bec0799728bbb224ea7f1
mhg1007 <bout77@naver.com>
2025-11-25 08:39:02 +0900

    baekjoon10829

---
+++ src/main/java/baekjoon/B10829.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B10829 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print(Long.toBinaryString(s.nextLong()));
+    }
+}
