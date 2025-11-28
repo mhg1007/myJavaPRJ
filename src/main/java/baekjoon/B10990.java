dac14975bec6e1816b3a76dd5fd23d1ffe404745
mhg1007 <bout77@naver.com>
2025-11-28 09:02:53 +0900

    baekjoon10990

---
+++ src/main/java/baekjoon/B10990.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B10990 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N+i; j++) {
+                if(j==N-i-1 || j==N+i-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
