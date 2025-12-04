2cc80641d1abf9855ce61cbb7c97ac0f7c0f870d
mhg1007 <bout77@naver.com>
2025-12-05 08:45:05 +0900

    baekjoon11320

---
+++ src/main/java/baekjoon/B11320.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B11320 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for (int i = 0; i < T; i++) {
+            int A=s.nextInt();
+            int B=s.nextInt();
+            System.out.println(A*A/(B*B));
+        }
+    }
+}
