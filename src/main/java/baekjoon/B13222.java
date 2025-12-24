94e039f50738dbec8062798b62fa8f54d06d0393
mhg1007 <bout77@naver.com>
2025-12-24 14:08:33 +0900

    baekjoon13222

---
+++ src/main/java/baekjoon/B13222.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B13222 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        int W=s.nextInt();
+        int H=s.nextInt();
+        double d=Math.sqrt(W*W+H*H);
+        for (int i = 0; i < N; i++) {
+            int n=s.nextInt();
+            System.out.println(n<=d?"YES":"NO");
+        }
+    }
+}
