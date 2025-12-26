5b1c2ee4d66d403c5481792227de6aa6f97c46f5
mhg1007 <bout77@naver.com>
2025-12-26 14:55:22 +0900

    baekjoon13235

---
+++ src/main/java/baekjoon/B13235.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B13235 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        StringBuilder sb=new StringBuilder();
+        sb.append(str).reverse();
+        System.out.print(str.contentEquals(sb)?"true":"false");
+    }
+}
