666b755c60581947164f93b0787a14a85c4e739d
mhg1007 <bout77@naver.com>
2025-12-26 14:57:14 +0900

    baekjoon13235

--- src/main/java/baekjoon/B13235.java
+++ src/main/java/baekjoon/B13235.java
@@ -6,8 +6,6 @@ public class B13235 {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         String str=s.next();
-        StringBuilder sb=new StringBuilder();
-        sb.append(str).reverse();
-        System.out.print(str.contentEquals(sb));
+        System.out.print(str.contentEquals(new StringBuilder(str).reverse()));
     }
 }
