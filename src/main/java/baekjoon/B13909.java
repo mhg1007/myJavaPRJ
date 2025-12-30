7896d43a8d88d4aadc82f084ea7408f648128f3a
mhg1007 <bout77@naver.com>
2025-12-30 15:17:12 +0900

    baekjoon13909

---
+++ src/main/java/baekjoon/B13909.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B13909 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print((int)Math.sqrt(s.nextInt()));
+    }
+}
