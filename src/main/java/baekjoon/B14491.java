86060192d2226d315b235c87e8df8f899d4d5844
mhg1007 <bout77@naver.com>
2025-12-31 15:09:40 +0900

    baekjoon14491

---
+++ src/main/java/baekjoon/B14491.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B14491 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print(Integer.toString(s.nextInt(),9));
+    }
+}
