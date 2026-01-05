6e454567fffb60d1ca9f25223464e2a20da99559
mhg1007 <bout77@naver.com>
2026-01-05 15:33:34 +0900

    baekjoon14581

---
+++ src/main/java/baekjoon/B14581.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B14581 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print(":fan::fan::fan:\n:fan::"+s.next()+"::fan:\n:fan::fan::fan:");
+    }
+}
