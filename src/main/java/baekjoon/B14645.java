4ea5edc9f84c43e1804612db9f9b09ab02d7ef96
mhg1007 <bout77@naver.com>
2026-01-05 15:37:44 +0900

    baekjoon14645

---
+++ src/main/java/baekjoon/B14645.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B14645 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        int K=s.nextInt();
+        for (int i = 0; i < N; i++) {
+            int A=s.nextInt();
+            int B=s.nextInt();
+        }
+        System.out.print("비와이");
+    }
+}
