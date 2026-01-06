071ef67a700fc133c0b41af31b19f8fbf7e13720
mhg1007 <bout77@naver.com>
2026-01-06 14:47:52 +0900

    baekjoon14935

---
+++ src/main/java/baekjoon/B14935.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B14935 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        BigInteger x=new BigInteger(s.next());
+        System.out.print("FA");
+    }
+}
