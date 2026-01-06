0979a6283dbadd8ecb3482c92f79a5b3bb616b46
mhg1007 <bout77@naver.com>
2026-01-06 14:54:09 +0900

    baekjoon15727

---
+++ src/main/java/baekjoon/B15727.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B15727 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int L=s.nextInt();
+        System.out.println((int)Math.ceil(L/5.0));
+    }
+}
