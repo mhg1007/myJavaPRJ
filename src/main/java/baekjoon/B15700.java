7c105993215b11278d692b254683370038925f38
mhg1007 <bout77@naver.com>
2026-01-06 14:52:46 +0900

    baekjoon15700

---
+++ src/main/java/baekjoon/B15700.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B15700 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        long N=s.nextLong();
+        long M=s.nextLong();
+        System.out.print((N*M)/2);
+    }
+}
