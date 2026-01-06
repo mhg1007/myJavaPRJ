65996bbe756eadc2ff34f85550db1d7ce5cad8e6
mhg1007 <bout77@naver.com>
2026-01-06 14:52:16 +0900

    baekjoon15680

---
+++ src/main/java/baekjoon/B15680.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B15680 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print(s.nextInt()==0?"YONSEI":"Leading the Way to the Future");
+    }
+}
