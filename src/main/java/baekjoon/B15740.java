ac67f34209668029a8bc9c0cf731c6ffa4373862
mhg1007 <bout77@naver.com>
2026-01-07 15:23:32 +0900

    baekjoon15740

---
+++ src/main/java/baekjoon/B15740.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B15740 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        BigInteger A=new BigInteger(s.next());
+        BigInteger B=new BigInteger(s.next());
+        System.out.println(A.add(B));
+    }
+}
