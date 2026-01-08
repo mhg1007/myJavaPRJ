574b0db4cf51d43c4d0b61c2bfed20136a578e64
mhg1007 <bout77@naver.com>
2026-01-08 13:49:20 +0900

    baekjoon16483

---
+++ src/main/java/baekjoon/B16483.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B16483 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        System.out.print((int)Math.round(T*T/4.0));
+    }
+}
