0dab446e67f28c86aaa21b215f390e803afc94ef
mhg1007 <bout77@naver.com>
2026-01-09 14:24:34 +0900

    baekjoon17219

---
+++ src/main/java/baekjoon/B17219.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class B17219 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+        Map<String,String> map=new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            st=new StringTokenizer(br.readLine());
+            map.put(st.nextToken(),st.nextToken());
+        }
+        for (int i = 0; i < M; i++) {
+            st=new StringTokenizer(br.readLine());
+            bw.write(map.get(st.nextToken())+"\n");
+        }
+        bw.flush();
+    }
+}
