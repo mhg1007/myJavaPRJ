e1c60d9ac261dccc615b150dffbbb481da4bb283
mhg1007 <bout77@naver.com>
2026-01-12 11:13:50 +0900

    baekjoon17863

---
+++ src/main/java/baekjoon/B17863.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B17863 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print(s.nextInt()/10000==555?"YES":"NO");
+    }
+}
