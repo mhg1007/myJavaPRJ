0eec0e3cbb1bf920323b19bbad9b4306aa95f8e5
mhg1007 <bout77@naver.com>
2026-01-13 14:53:29 +0900

    baekjoon18406

--- src/main/java/baekjoon/B18406.java
+++ src/main/java/baekjoon/B18406.java
@@ -7,13 +7,13 @@ public class B18406 {
         Scanner s=new Scanner(System.in);
         int N=s.nextInt();
         String NN=String.valueOf(N);
-        int len=NN.length();
+        int ln=NN.length();
         int sum1=0,sum2=0;
-        for (int i = 0; i < NN.length()/2; i++) {
-            sum1+=Integer.parseInt(String.valueOf(NN.charAt(i)));
+        for (int i = 0; i < ln/2; i++) {
+            sum1+=Character.getNumericValue(NN.charAt(i));
         }
-        for (int i = NN.length()/2; i < NN.length(); i++) {
-            sum2+=Integer.parseInt(String.valueOf(NN.charAt(i)));
+        for (int i = ln/2; i < ln; i++) {
+            sum2+=Character.getNumericValue(NN.charAt(i));
         }
         if(sum1==sum2){
             System.out.print("LUCKY");
