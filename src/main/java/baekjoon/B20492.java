aec188f7028243b788d89b63029dfc34e47a2ff6
mhg1007 <bout77@naver.com>
2026-01-14 16:37:27 +0900

    baekjoon20492

---
+++ src/main/java/baekjoon/B20492.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B20492 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        System.out.print((int)(N*0.78)+" "+(int)(N*0.956));
+    }
+}
