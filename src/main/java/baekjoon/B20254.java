5ebc5a66029f49ac8f33c4c81cb65a38dca0cf75
mhg1007 <bout77@naver.com>
2026-01-14 16:35:02 +0900

    baekjoon20254

---
+++ src/main/java/baekjoon/B20254.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B20254 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print(s.nextInt()*56+s.nextInt()*24+s.nextInt()*14+s.nextInt()*6);
+    }
+}
