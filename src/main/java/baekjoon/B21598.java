bb2080c7bf7e792e1f7f1e0c5b8dae4b6e238dae
mhg1007 <bout77@naver.com>
2026-01-15 14:04:49 +0900

    baekjoon21598

---
+++ src/main/java/baekjoon/B21598.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B21598 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        for (int i = 0; i < N; i++) {
+            System.out.println("SciComLove");
+        }
+    }
+}
